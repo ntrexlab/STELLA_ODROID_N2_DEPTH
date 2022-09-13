17aa6ae5f8da38b2265641ac3a9e6670b2aa6b8a
ntrexlab <85467544+ntrexlab@users.noreply.github.com>
2022-09-13 13:45:30 +0900

    Add files via upload

--- stella_bringup/create_udev_rules.sh
+++ stella_bringup/create_udev_rules.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "remap the devices serial port(ttyUSBX, ttySX) to  ydlidar, Astra Pro, AHRS, Motordriver, Bluetooth"
-echo "devices usb connection as /dev/YDLIDAR, /dev/AHRS, /dev/MW, /dev/BT, /dev/astra_pro, check it using the command : ls -l /dev|grep -e ttyUSB -e ttyS0"
+echo "remap the devices serial port(ttyUSBX, ttySX) to  ydlidar, Astra, AHRS, Motordriver, Bluetooth"
+echo "devices usb connection as /dev/YDLIDAR, /dev/AHRS, /dev/MW, /dev/BT, /dev/astra, check it using the command : ls -l /dev|grep -e ttyUSB -e ttyS"
 echo "start copy stella.rules to  /etc/udev/rules.d/"
 echo "`rospack find stella_bringup`/stella.rules"
 sudo cp `rospack find stella_bringup`/stella.rules  /etc/udev/rules.d
+echo "start copy camera_info to  ~/.ros"
+echo "`rospack find stella_bringup`/camera_info"
+sudo cp -r `rospack find stella_bringup`/camera_info ~/.ros
 echo " "
 echo "Restarting udev"
 echo ""
